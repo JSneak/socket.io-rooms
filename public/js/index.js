29df5c065f190703aaa12f27a228b423f98ad9ea
JSneak <kim2087@students.d211.org>
2018-02-20 20:51:36 -0600

    socket.io rooms joining and leaving

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,21 @@
 var socket = io();
 
 socket.on("join room 1", function(data){
+  transition(true);
   console.log("Joined Room 1")
 });
 
 socket.on("join room 2", function(data){
+  transition(true);
   console.log("Joined Room 2")
+});
+
+socket.on("leave room 1", function(data) {
+  transition(false);
+});
+
+socket.on("leave room 2", function(data) {
+  transition(false);
 });
 
 function joinRoom1() {
@@ -15,3 +25,21 @@ function joinRoom1() {
 function joinRoom2() {
   socket.emit("join room 2");
 };
+
+function leaveRoom1() {
+  socket.emit("leave room 1");
+};
+
+function leaveRoom2() {
+  socket.emit("leave room 2");
+};
+
+function transition(state) {
+  if(state == true) {
+    document.getElementById("join").className = "hidden";
+    document.getElementById("leave").className = "visible";
+  }else{
+    document.getElementById("join").className = "visible";
+    document.getElementById("leave").className = "hidden";
+  }
+}
--- server.js
+++ server.js
@@ -9,12 +9,28 @@ io.on('connection', function(socket) {
 
   socket.on("join room 1", function(data) {
     socket.join("room 1");
-    io.to('room 1').emit('join room 1');
+    socket.emit("join room 1");
+    //Broadcast to all sockets in room 1
+    //io.to('room 1').emit('join room 1');
   });
 
   socket.on("join room 2", function(data) {
     socket.join("room 2");
-    io.to('room 2').emit('join room 2');
+    socket.emit("join room 2");
+    //Broadcast to all sockets in room 2
+    //io.to('room 2').emit('join room 2');
+  });
+
+  socket.on("leave room 1", function(data) {
+    socket.leave("room 1");
+    socket.emit("leave room 1");
+    //io.to('room 1').emit('join room 1');
+  });
+
+  socket.on("leave room 2", function(data) {
+    socket.leave("room 2");
+    socket.emit("leave room 2");
+    //io.to('room 2').emit('join room 2');
   });
 
 });
